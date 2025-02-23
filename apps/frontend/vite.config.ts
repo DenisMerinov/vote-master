44b8e7a80c6ee3c8f167f602429b0b7e4e1ee379
DenisM <wow21moon@yandex.ru>
2025-02-23 12:40:52 +0700

    fix(vite): added env

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,38 +1,42 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
-export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/frontend',
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  build: {
-    outDir: '../../dist',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ''); // Загружаем переменные окружения
+
+  return {
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/apps/frontend',
+    server: {
+      port: 4200,
+      host: env.VITE_HOST || 'localhost',
     },
-  },
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../../coverage/apps/frontend',
-      provider: 'v8',
+    preview: {
+      port: 4300,
+      host: env.VITE_HOST || 'localhost',
     },
-  },
+    plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+    build: {
+      outDir: '../../dist',
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
+    test: {
+      watch: false,
+      globals: true,
+      environment: 'jsdom',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      reporters: ['default'],
+      coverage: {
+        reportsDirectory: '../../coverage/apps/frontend',
+        provider: 'v8',
+      },
+    },
+  };
 });
